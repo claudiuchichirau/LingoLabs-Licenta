f30d7415d31563009cc70672d3e84ceec16b271b
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2024-02-19 20:04:32 +0200

    test commit

--- LingoLabs/LingoLabs.Application/obj/Debug/net8.0/LingoLabs.Application.AssemblyInfo.cs
+++ LingoLabs/LingoLabs.Application/obj/Debug/net8.0/LingoLabs.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LingoLabs.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d32451078225cd0bdd1d63198a5a6ef86eb46d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LingoLabs.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LingoLabs.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LingoLabs/LingoLabs.Domain/obj/Debug/net8.0/LingoLabs.Domain.AssemblyInfo.cs
+++ LingoLabs/LingoLabs.Domain/obj/Debug/net8.0/LingoLabs.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LingoLabs.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d32451078225cd0bdd1d63198a5a6ef86eb46d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LingoLabs.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LingoLabs.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
