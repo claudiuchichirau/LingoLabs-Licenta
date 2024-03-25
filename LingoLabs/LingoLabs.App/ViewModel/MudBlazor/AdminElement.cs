af34edcecf3af9811e39992610a11985bfc0a408
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2024-03-25 22:45:23 +0200

    ui for dashboard page (student + admin)

---
+++ LingoLabs/LingoLabs.App/ViewModel/MudBlazor/AdminElement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace LingoLabs.App.ViewModel.MudBlazor
+{
+    public class AdminElement
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ LingoLabs/LingoLabs.App/ViewModel/MudBlazor/AdminViewLanguageElement.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LingoLabs.App.ViewModel.MudBlazor
+{
+    public class AdminViewLanguageElement
+    {
+        public string LanguageName { get; set; } = string.Empty;
+        public string? LanguageDescription { get; set; } 
+        public string? LanguageVideoLink { get; set; }
+        public string? LanguageFlag { get; set; }
+    }
+}
