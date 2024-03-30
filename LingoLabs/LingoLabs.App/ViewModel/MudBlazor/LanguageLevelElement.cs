2b21b2247c091a70db7553ed901779c4f29c23de
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2024-03-30 18:14:35 +0200

    continue with ui for Language Overview page

---
+++ LingoLabs/LingoLabs.App/ViewModel/MudBlazor/LanguageLevelElement.cs
@@ -0,0 +1,11 @@
+﻿namespace LingoLabs.App.ViewModel.MudBlazor
+{
+    public class LanguageLevelElement
+    {
+        public string LanguageLevelName { get; set; } = string.Empty;
+        public string LanguageLevelAlias { get; set; } = string.Empty;
+        public string? LanguageLevelDescription { get; set; }
+        public string? LanguageLevelVideoLink { get; set; }
+        public int? LanguageLevelPriorityNumber { get; set; }
+    }
+}
