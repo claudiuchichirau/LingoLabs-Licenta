78a7dfd5eacb08f7546a23da08b94d339bed6034
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2024-03-31 23:29:21 +0300

    almost finish ui for Language Overview

---
+++ LingoLabs/LingoLabs.App/ViewModel/MudBlazor/ChoiceElement.cs
@@ -0,0 +1,8 @@
+﻿namespace LingoLabs.App.ViewModel.MudBlazor
+{
+    public class ChoiceElement
+    {
+        public string ChoiceContent { get; set; } = string.Empty;
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ LingoLabs/LingoLabs.App/ViewModel/MudBlazor/QuestionElement.cs
@@ -0,0 +1,11 @@
+﻿namespace LingoLabs.App.ViewModel.MudBlazor
+{
+    public class QuestionElement
+    {
+        public Guid QuestionId { get; set; }
+        public string QuestionRequirement { get; set; } = string.Empty;
+        public List<ChoiceElement>? QuestionChoices { get; set; }
+        public string? QuestionImageData { get; set; }
+        public string? QuestionVideoLink { get; set; } 
+    }
+}
