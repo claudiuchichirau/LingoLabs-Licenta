5e4f58527ed29fbfec75ee35ed47607b8b775117
claudiuchichirau <chichirau.claudiu@yahoo.ro>
2024-04-12 14:34:42 +0300

    finish ui for about us, contact page

--- LingoLabs/LingoLabs.Infrastructure/obj/Debug/net8.0/LingoLabs.Infrastructure.AssemblyInfo.cs
+++ LingoLabs/LingoLabs.Infrastructure/obj/Debug/net8.0/LingoLabs.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LingoLabs.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75ff21b6304fb18dfc0158a7c8b727522d89250c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9760dc02968383fb98cf3ae5172f574794b3525")]
 [assembly: System.Reflection.AssemblyProductAttribute("LingoLabs.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LingoLabs.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
